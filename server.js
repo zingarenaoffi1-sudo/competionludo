093d76c734a44d7a39c0946f9f9fe9e4c3394eb5
zingarenaoffi1-sudo <zingarenaoffi1@gmail.com>
2026-08-23 20:15:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -407,6 +407,6 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Ludo Master Server running on port ${PORT}`);
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Ludo Master SECURE Server running on port ${PORT}`);
 });
